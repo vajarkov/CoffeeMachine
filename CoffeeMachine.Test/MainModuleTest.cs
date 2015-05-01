996f436a1718cce875969ae353bb3cc3df8971c1
vajarkov <vajarkov@mail.ru>
2015-05-01 17:10:44 +0600

    Прошел moq-тест

--- CoffeeMachine.Test/MainModuleTest.cs
+++ CoffeeMachine.Test/MainModuleTest.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Text;
-using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 
@@ -37,32 +35,11 @@ namespace CoffeeMachine.Test
             }
         }
 
-        #region Additional test attributes
-        //
-        // You can use the following additional attributes as you write your tests:
-        //
-        // Use ClassInitialize to run code before running the first test in the class
-        // [ClassInitialize()]
-        // public static void MyClassInitialize(TestContext testContext) { }
-        //
-        // Use ClassCleanup to run code after all tests in a class have run
-        // [ClassCleanup()]
-        // public static void MyClassCleanup() { }
-        //
-        // Use TestInitialize to run code before running each test 
-        // [TestInitialize()]
-        // public void MyTestInitialize() { }
-        //
-        // Use TestCleanup to run code after each test has run
-        // [TestCleanup()]
-        // public void MyTestCleanup() { }
-        //
-        #endregion
 
         [TestMethod]
         public void FillWaterTest()
         {
-            /*
+            
             // Создаем mock-объект
             var waterModule = new Mock<IWaterModule>();
             var coffeeModule = new Mock<ICoffeeModule>();
@@ -71,7 +48,7 @@ namespace CoffeeMachine.Test
             var mainModule = new MainModule(waterModule.Object, garbageModule.Object, coffeeModule.Object);
             // Вызываем метод, который мы тестируем
             mainModule.FillWater();
-            waterModule.Verify(wm => wm.FillWater());*/
+            waterModule.Verify(wm => wm.FillWater());
         }
     }
 }
--- CoffeeMachine/MainModule.cs
+++ CoffeeMachine/MainModule.cs
@@ -3,10 +3,33 @@
 
 namespace CoffeeMachine
 {
-    class MainModule 
+    
+    public class MainModule 
     {
-        public MainModule(object wm, object gm, object cm)
+        private ICoffeeModule _coffeeModule;
+        private IWaterModule _waterModule;
+        private IGarbageModule _garbageModule;
+
+        public MainModule(IWaterModule wm, IGarbageModule gm, ICoffeeModule cm)
         {
+            _coffeeModule = cm;
+            _waterModule = wm;
+            _garbageModule = gm;
+        }
+
+        public void FillWater()
+        {
+            _waterModule.FillWater();
+        }
+
+        public void FillCoffee()
+        {
+            _coffeeModule.FillCoffee();
+        }
+
+        public void Clear()
+        {
+            _garbageModule.Clear();
         }
     }
 }
